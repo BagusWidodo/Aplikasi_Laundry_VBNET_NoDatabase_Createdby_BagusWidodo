31dcaeab92adcc0a7d296b65b1a43490a913d612
BagusWidodo <46666642+BagusWidodo@users.noreply.github.com>
2021-07-03 22:21:14 +0700

    Add files via upload

---
+++ Aplikasi_Laundry/Form1.Designer.vb
@@ -0,0 +1,420 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.t_id = New System.Windows.Forms.TextBox()
+        Me.t_nama = New System.Windows.Forms.TextBox()
+        Me.t_alamat = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.t_nomor = New System.Windows.Forms.TextBox()
+        Me.t_ketpakaian = New System.Windows.Forms.TextBox()
+        Me.t_hargakg = New System.Windows.Forms.TextBox()
+        Me.t_jlhsatuan = New System.Windows.Forms.TextBox()
+        Me.cb_pewangi = New System.Windows.Forms.ComboBox()
+        Me.t_total = New System.Windows.Forms.TextBox()
+        Me.rd_reguler = New System.Windows.Forms.RadioButton()
+        Me.rd_express = New System.Windows.Forms.RadioButton()
+        Me.btn_hitung = New System.Windows.Forms.Button()
+        Me.btn_reset = New System.Windows.Forms.Button()
+        Me.btn_keluar = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.t_hargapewangi = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Cambria", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(250, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(180, 22)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "APLIKASI LAUNDRY"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(26, 73)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "ID Pelanggan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(26, 104)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(97, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Nama Pelanggan"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(26, 135)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 15)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Alamat"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(26, 168)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 15)
+        Me.Label5.TabIndex = 1
+        Me.Label5.Text = "Tanggal"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(26, 197)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(92, 15)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "No. Handphone"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(26, 226)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(74, 15)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "Ket. Pakaian"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(339, 73)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(83, 15)
+        Me.Label8.TabIndex = 1
+        Me.Label8.Text = "Jenis Layanan"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(339, 101)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(65, 15)
+        Me.Label9.TabIndex = 1
+        Me.Label9.Text = "Harga / Kg"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(339, 132)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(85, 15)
+        Me.Label10.TabIndex = 1
+        Me.Label10.Text = "Jumlah Satuan"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(339, 162)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(53, 15)
+        Me.Label11.TabIndex = 1
+        Me.Label11.Text = "Pewangi"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(339, 225)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(90, 15)
+        Me.Label12.TabIndex = 1
+        Me.Label12.Text = "TOTAL HARGA"
+        '
+        't_id
+        '
+        Me.t_id.Location = New System.Drawing.Point(149, 69)
+        Me.t_id.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_id.Name = "t_id"
+        Me.t_id.Size = New System.Drawing.Size(168, 23)
+        Me.t_id.TabIndex = 2
+        '
+        't_nama
+        '
+        Me.t_nama.Location = New System.Drawing.Point(149, 100)
+        Me.t_nama.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_nama.Name = "t_nama"
+        Me.t_nama.Size = New System.Drawing.Size(168, 23)
+        Me.t_nama.TabIndex = 3
+        '
+        't_alamat
+        '
+        Me.t_alamat.Location = New System.Drawing.Point(149, 131)
+        Me.t_alamat.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_alamat.Name = "t_alamat"
+        Me.t_alamat.Size = New System.Drawing.Size(168, 23)
+        Me.t_alamat.TabIndex = 3
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(149, 161)
+        Me.DateTimePicker1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(168, 23)
+        Me.DateTimePicker1.TabIndex = 4
+        '
+        't_nomor
+        '
+        Me.t_nomor.Location = New System.Drawing.Point(149, 192)
+        Me.t_nomor.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_nomor.Name = "t_nomor"
+        Me.t_nomor.Size = New System.Drawing.Size(168, 23)
+        Me.t_nomor.TabIndex = 3
+        '
+        't_ketpakaian
+        '
+        Me.t_ketpakaian.Location = New System.Drawing.Point(149, 223)
+        Me.t_ketpakaian.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_ketpakaian.Multiline = True
+        Me.t_ketpakaian.Name = "t_ketpakaian"
+        Me.t_ketpakaian.Size = New System.Drawing.Size(168, 72)
+        Me.t_ketpakaian.TabIndex = 3
+        '
+        't_hargakg
+        '
+        Me.t_hargakg.Location = New System.Drawing.Point(470, 97)
+        Me.t_hargakg.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_hargakg.Name = "t_hargakg"
+        Me.t_hargakg.Size = New System.Drawing.Size(175, 23)
+        Me.t_hargakg.TabIndex = 3
+        '
+        't_jlhsatuan
+        '
+        Me.t_jlhsatuan.Location = New System.Drawing.Point(470, 128)
+        Me.t_jlhsatuan.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_jlhsatuan.Name = "t_jlhsatuan"
+        Me.t_jlhsatuan.Size = New System.Drawing.Size(111, 23)
+        Me.t_jlhsatuan.TabIndex = 3
+        '
+        'cb_pewangi
+        '
+        Me.cb_pewangi.FormattingEnabled = True
+        Me.cb_pewangi.Items.AddRange(New Object() {"Downy", "Kispray", "So Klin", "Rinso"})
+        Me.cb_pewangi.Location = New System.Drawing.Point(470, 159)
+        Me.cb_pewangi.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.cb_pewangi.Name = "cb_pewangi"
+        Me.cb_pewangi.Size = New System.Drawing.Size(175, 23)
+        Me.cb_pewangi.TabIndex = 5
+        '
+        't_total
+        '
+        Me.t_total.Location = New System.Drawing.Point(470, 221)
+        Me.t_total.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_total.Name = "t_total"
+        Me.t_total.Size = New System.Drawing.Size(175, 23)
+        Me.t_total.TabIndex = 6
+        '
+        'rd_reguler
+        '
+        Me.rd_reguler.AutoSize = True
+        Me.rd_reguler.Location = New System.Drawing.Point(470, 70)
+        Me.rd_reguler.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rd_reguler.Name = "rd_reguler"
+        Me.rd_reguler.Size = New System.Drawing.Size(67, 19)
+        Me.rd_reguler.TabIndex = 7
+        Me.rd_reguler.TabStop = True
+        Me.rd_reguler.Text = "Reguler"
+        Me.rd_reguler.UseVisualStyleBackColor = True
+        '
+        'rd_express
+        '
+        Me.rd_express.AutoSize = True
+        Me.rd_express.Location = New System.Drawing.Point(574, 70)
+        Me.rd_express.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.rd_express.Name = "rd_express"
+        Me.rd_express.Size = New System.Drawing.Size(68, 19)
+        Me.rd_express.TabIndex = 7
+        Me.rd_express.TabStop = True
+        Me.rd_express.Text = "Express"
+        Me.rd_express.UseVisualStyleBackColor = True
+        '
+        'btn_hitung
+        '
+        Me.btn_hitung.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_hitung.Location = New System.Drawing.Point(343, 261)
+        Me.btn_hitung.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_hitung.Name = "btn_hitung"
+        Me.btn_hitung.Size = New System.Drawing.Size(87, 34)
+        Me.btn_hitung.TabIndex = 8
+        Me.btn_hitung.Text = "HITUNG"
+        Me.btn_hitung.UseVisualStyleBackColor = True
+        '
+        'btn_reset
+        '
+        Me.btn_reset.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_reset.Location = New System.Drawing.Point(450, 261)
+        Me.btn_reset.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_reset.Name = "btn_reset"
+        Me.btn_reset.Size = New System.Drawing.Size(87, 34)
+        Me.btn_reset.TabIndex = 8
+        Me.btn_reset.Text = "RESET"
+        Me.btn_reset.UseVisualStyleBackColor = True
+        '
+        'btn_keluar
+        '
+        Me.btn_keluar.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_keluar.Location = New System.Drawing.Point(558, 261)
+        Me.btn_keluar.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btn_keluar.Name = "btn_keluar"
+        Me.btn_keluar.Size = New System.Drawing.Size(87, 34)
+        Me.btn_keluar.TabIndex = 8
+        Me.btn_keluar.Text = "KELUAR"
+        Me.btn_keluar.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(587, 131)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(58, 15)
+        Me.Label13.TabIndex = 1
+        Me.Label13.Text = "Kilogram"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(467, 195)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(39, 15)
+        Me.Label14.TabIndex = 1
+        Me.Label14.Text = "Harga"
+        '
+        't_hargapewangi
+        '
+        Me.t_hargapewangi.Location = New System.Drawing.Point(512, 190)
+        Me.t_hargapewangi.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.t_hargapewangi.Name = "t_hargapewangi"
+        Me.t_hargapewangi.Size = New System.Drawing.Size(133, 23)
+        Me.t_hargapewangi.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(668, 322)
+        Me.Controls.Add(Me.btn_keluar)
+        Me.Controls.Add(Me.btn_reset)
+        Me.Controls.Add(Me.btn_hitung)
+        Me.Controls.Add(Me.rd_express)
+        Me.Controls.Add(Me.rd_reguler)
+        Me.Controls.Add(Me.t_hargapewangi)
+        Me.Controls.Add(Me.t_total)
+        Me.Controls.Add(Me.cb_pewangi)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.t_jlhsatuan)
+        Me.Controls.Add(Me.t_hargakg)
+        Me.Controls.Add(Me.t_ketpakaian)
+        Me.Controls.Add(Me.t_nomor)
+        Me.Controls.Add(Me.t_alamat)
+        Me.Controls.Add(Me.t_nama)
+        Me.Controls.Add(Me.t_id)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Cambria", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "APLIKASI LAUNDRY"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents t_id As System.Windows.Forms.TextBox
+    Friend WithEvents t_nama As System.Windows.Forms.TextBox
+    Friend WithEvents t_alamat As System.Windows.Forms.TextBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents t_nomor As System.Windows.Forms.TextBox
+    Friend WithEvents t_ketpakaian As System.Windows.Forms.TextBox
+    Friend WithEvents t_hargakg As System.Windows.Forms.TextBox
+    Friend WithEvents t_jlhsatuan As System.Windows.Forms.TextBox
+    Friend WithEvents cb_pewangi As System.Windows.Forms.ComboBox
+    Friend WithEvents t_total As System.Windows.Forms.TextBox
+    Friend WithEvents rd_reguler As System.Windows.Forms.RadioButton
+    Friend WithEvents rd_express As System.Windows.Forms.RadioButton
+    Friend WithEvents btn_hitung As System.Windows.Forms.Button
+    Friend WithEvents btn_reset As System.Windows.Forms.Button
+    Friend WithEvents btn_keluar As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents t_hargapewangi As System.Windows.Forms.TextBox
+
+End Class
---
+++ Aplikasi_Laundry/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+    Private Sub rd_reguler_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rd_reguler.CheckedChanged
+        If rd_reguler.Checked = True Then
+            t_hargakg.Text = 4000
+        End If
+    End Sub
+
+    Private Sub rd_express_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rd_express.CheckedChanged
+        If rd_express.Checked = True Then
+            t_hargakg.Text = 6000
+        End If
+    End Sub
+
+    Private Sub cb_pewangi_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cb_pewangi.SelectedIndexChanged
+        If cb_pewangi.Text = "Downy" Then
+            t_hargapewangi.Text = 2000
+        ElseIf cb_pewangi.Text = "Kispray" Then
+            t_hargapewangi.Text = 2500
+        ElseIf cb_pewangi.Text = "So Klin" Then
+            t_hargapewangi.Text = 2500
+        Else
+            t_hargapewangi.Text = 1500
+        End If
+    End Sub
+
+    Private Sub btn_hitung_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_hitung.Click
+        t_total.Text = (Val(t_hargakg.Text) * Val(t_jlhsatuan.Text)) + Val(t_hargapewangi.Text)
+    End Sub
+
+    Private Sub btn_reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_reset.Click
+
+        t_id.Text = ""
+        t_nama.Text = ""
+        t_alamat.Text = ""
+        t_nomor.Text = ""
+        t_ketpakaian.Text = ""
+        rd_reguler.Checked = False
+        rd_express.Checked = False
+        t_hargakg.Text = ""
+        t_jlhsatuan.Text = ""
+        cb_pewangi.Text = ""
+        t_hargapewangi.Text = ""
+        t_total.Text = ""
+        t_id.Focus()
+
+
+    End Sub
+
+    Private Sub btn_keluar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_keluar.Click
+        End
+    End Sub
+
+End Class
